d4990611a8352ed51f616a0f10ac52fb64d36244
ktoyod <ytdh7834@gmail.com>
2023-02-05 22:44:03 +0900

    Update description of variables.tf

--- terraform/aws/environments/shared/variables.tf
+++ terraform/aws/environments/shared/variables.tf
@@ -1,5 +1,5 @@
 variable "github_repos" {
-  description = "The list of GitHub repositories to create"
+  description = "The list of GitHub repositories allowed to assume the role"
   type        = list(string)
   default     = []
 }
